8ec3ed84d70558a6d18a5c7709c27b32380b44ec
mrdavidal <contis.ali@gmail.com>
2022-12-07 19:25:51 +0200

    Chunks of lists from list

---
+++ chunk.py
@@ -0,0 +1,24 @@
+"""
+You have a lot of work to do, so you might want to split it into smaller pieces.
+This way you'll know which piece you'll do on Monday, which will be for Tuesday and so on.
+
+Split a list into smaller lists of the same size (chunks).
+The last chunk can be smaller than the default chunk-size. If the list is empty,
+then you shouldn't have any chunks at all.
+
+
+Input: Two arguments. A List and chunk size.
+
+Output: An Iterable with chunked Iterable.
+"""
+from typing import Iterable
+
+
+def chunking_by(items: list, size: int) -> Iterable:
+    # your code here
+    for i in range(0, len(items), size):
+        yield items[i : i + size]
+
+
+print("Example:")
+print(list(chunking_by([5, 4, 7, 3, 4, 5, 4], 3)))
